3477556cc4ae126c57888c485f132c4a7471a1e1
Nick Law <89273070+OzenAmare@users.noreply.github.com>
2026-05-02 18:21:48 -0400

    started building classes and such to get the ball rolling

---
+++ Models/Items.cs
@@ -0,0 +1,11 @@
+namespace Item
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rarity { get; set; }
+        public string Type { get; set; }
+    }
+}
