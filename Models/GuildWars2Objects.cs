38790a714b2ed3323befe81f9f02b6be34e418f2
Nick Law <89273070+OzenAmare@users.noreply.github.com>
2026-05-02 18:35:57 -0400

    started item class with interface

---
+++ Models/GuildWars2Objects.cs
@@ -0,0 +1,15 @@
+using GuildWars2Object.Models;
+
+namespace Item
+{
+    public class Item : IGuildWars2Object
+    {
+        public async Task GetObjectData()
+        {
+           // Implementation for getting item data from GW2 API
+        }
+    }
+
+    
+    
+}
--- Models/IGuildWars2Object.cs
+++ Models/IGuildWars2Object.cs
@@ -1,12 +1,12 @@
 using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
 
-namespace GuildWars2Object
+namespace GuildWars2Object.Models
 {
     public interface IGuildWars2Object
     {
         //we'll need to use this for the different things in GW2 
-        public async Task GetObjectData();
+        public async Task GetObjectData() { }
 
         
     }
