bc2fdb70fe298ff79421212e7ddc74c2fb65bb6e
Nick Law <89273070+OzenAmare@users.noreply.github.com>
2026-05-02 23:27:54 -0400

    more writing out of the structure for an item in GW2

--- Models/Items.cs
+++ Models/Items.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace Items
 {
-    public class Item: IGuildWars2Object
+    public class Item : IGuildWars2Object
     {
         public async Task GetObjectData(int id)
         {
 
         }
-        
+
         //to finish this go to this https://api.guildwars2.com/v2/items/30704
         private string _item_id;
 
@@ -38,7 +38,7 @@ namespace Items
         private int _default_skin { get; set; }
 
         public int default_skin => _default_skin;
-        
+
         private Dictionary<int, string> _game_types { get; set; }
 
         public Dictionary<int, string> game_types => _game_types;
@@ -46,6 +46,42 @@ namespace Items
         private Dictionary<int, string> _flags { get; set; }
 
         public Dictionary<int, string> flags => _flags;
+
+        private List<string> _restrictions { get; set; }
+
+        public List<string> restrictions => _restrictions;
+
+        private string _chat_link { get; set; }
+
+        public string chat_link => _chat_link;
+
+
+        //this is literally a link to an icon. Maybe we could do something better?
+        private string _icon { get; set; }
+
+        public string icon => _icon;
+
+        
+    
+        protected class item_details
+        {
+            private string _item_type { get; set; }
+
+            public string item_type => _item_type; 
+
+            private string _damage_type { get; set; }
+
+            public string damage_type => _damage_type; 
+
+            private int _min_power { get; set; }
+
+            public int min_power => _min_power;
+
+            private int _max_power { get; set; }
+
+            public int max_power => _max_power; 
+        }
     }
+    
     
 }
